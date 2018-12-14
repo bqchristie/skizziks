84c8fd8ca4f0def7238cf14a3fb1c0dbeb2e076d
bqchristie <bqchristie@gmail.com>
2018-12-14 10:45:54 -0500

    change auth path

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,5 +2,5 @@
 module.exports = {
   NODE_ENV: '"production"',
   API_PATH: '"https://api.fuudlist.com/api"',
-  AUTH_PATH: '"https://api.fuudlist.com/api"'
+  AUTH_PATH: '"https://api.fuudlist.com/auth"'
 }
